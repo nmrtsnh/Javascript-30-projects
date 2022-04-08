79baa21f1b69d67e110bf8bc0b82f1a965e3e4be
Namrata Sinha <namratasinha26@gmail.com>
2022-04-08 17:05:40 +0200

    Checkboxes

---
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -0,0 +1,21 @@
+const checkBoxes = document.querySelectorAll('inbox, input[type="checkbox"]');
+// console.log(checkbox);
+
+function handleCheck(e) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkBoxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+checkBoxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleCheck)
+);

76fdc8456ea3872f39a7f7e5b3173a6aaa2f0c11
Namrata Sinha <namratasinha26@gmail.com>
2022-04-22 14:56:03 +0200

    Event capture, propagation, bubbling, and once

---
+++ 25 - Event Capture, Propagation, Bubbling and Once/script.js
@@ -0,0 +1,12 @@
+const divs = document.querySelectorAll("div");
+
+function logText(e) {
+  console.log(this.classList.value);
+  //   console.log(this);
+  e.stopPropagation();
+}
+
+divs.forEach((div) => div.addEventListener("click", logText), {
+  capture: false,
+  once: true,
+});
